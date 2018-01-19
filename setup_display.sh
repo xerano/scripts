898601f98624fbdc866b1569e6840546d3d3c1e3
xerano <mlehmann83@gmail.com>
2018-01-19 12:20:00 +0100

    Update setup_display.sh

--- setup_display.sh
+++ setup_display.sh
@@ -5,9 +5,13 @@ sleep 0.1
 LVDS_NAME=$(xrandr --query | grep "LVDS" | grep " connected" | cut -d ' ' -f 1)
 PREV=$LVDS_NAME
 
-xrandr --output $LVDS_NAME --auto
+CMD="--output $LVDS_NAME --auto"
 
 for output in `xrandr --query | grep "DP" | grep " connected" | cut -d ' ' -f 1`; do
-  xrandr --output $output --right-of $PREV
+  CMD="$CMD --output $output --right-of $PREV"
   PREV=$output
 done
+
+echo "xrandr $CMD"
+
+xrandr $CMD
